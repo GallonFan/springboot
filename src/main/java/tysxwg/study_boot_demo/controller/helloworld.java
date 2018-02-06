28dd8bc1bb6038cb9dcbb44a608857ef660d9992
Gallon_tv189 <fan073316@163.com>
2018-02-06 09:45:43 +0800

    修改配置文件

--- src/main/java/tysxwg/study_boot_demo/controller/helloworld.java
+++ src/main/java/tysxwg/study_boot_demo/controller/helloworld.java
@@ -1,6 +1,9 @@
 package tysxwg.study_boot_demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class helloworld {
+//    @Value("${test.msg}")
+//    private String msg;
+@Autowired
+private Environment env;
+
 
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String index(){
-        return "Hello World~";
+//        return "Hello World~"+msg;
+        return "方式二:"+env.getProperty("test.msg");
     }
 }
